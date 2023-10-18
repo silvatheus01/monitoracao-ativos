45b3997e19b581a179dc4f2de3a4479fc1464358
Matheus Silva <matheus.car.ms@gmail.com>
2023-10-18 17:43:04 -0300

    feat: regra de envio de e-mail alterada

--- cotacoes/monitoracao/tasks.py
+++ cotacoes/monitoracao/tasks.py
@@ -1,13 +1,15 @@
 from celery import shared_task
 from django.utils import timezone
 
-from django.db.models import Model
+from django.core.exceptions import ObjectDoesNotExist
 
 from monitoracao.models import Quotation, Monitor
 from .api.sheets import Spreadsheet    
 from django.core.mail import send_mail as send_email_django
 
 from django.conf import settings
+
+from math import isclose
 
 def send_email(msg):
     send_email_django(
@@ -19,46 +21,72 @@ def send_email(msg):
         html_message=msg
     )
 
-@shared_task(bind=True)
-def save_quote_monitoring(self, name, row):
-    try:
-        spreadsheet = Spreadsheet()
-        security = spreadsheet.get_security(row)
-    except Exception:
-        return 'Não foi possível acessar os dados da API.'
+def equals(num1, num2):
+    return isclose(num1, num2)
+
+def get_monitor(asset_name):
+    return Monitor.objects.get(asset__name=asset_name)
+
+def get_security(row):
+    spreadsheet = Spreadsheet()
+    return spreadsheet.get_security(row)
+
+def get_last_monitoring(asset_name):
+    return Quotation.objects.filter(asset__name=asset_name).order_by('-recording_date').first()
+
+def show_alert(upper_limit, lower_limit, price):
+    return (upper_limit < price or lower_limit > price) and not (equals(upper_limit, price) or equals(lower_limit, price))
     
+def handler_log(monitor, security, asset_name):
     price = security.price
-    tradetime = security.tradetime
-
-    try:
-        monitor = Monitor.objects.get(asset__name=name)
-    except Model.DoesNotExist:
-        return 'O ativo não está acessível'
-    
-    now = timezone.now()
-    
-    quotation = Quotation(
-        asset=monitor.asset, 
-        price=price, 
-        recording_date=now, 
-        trade_date=tradetime
-    )
-    quotation.save()
-
     upper_limit = monitor.upper_limit
     lower_limit = monitor.lower_limit
+   
+    msg_log = f'O preço do ativo {asset_name} é de R$ {price}'
+    email_msg = f'O preço do ativo <b>{asset_name}</b> é de <b>R$ {price}</b>'
 
-    msg_log = f'O preço do ativo {name} é de R$ {price}'
-    msg_email = f'O preço do ativo <b>{name}</b> é de <b>R$ {price}</b>'
-    if upper_limit < price:
-        alert = '. Esse é o momento de vender.'
+    if (show_alert(upper_limit, lower_limit, price)):
+        if upper_limit < price:
+            alert = '. Esse é o momento de vender.'
+        else:
+            alert = '. Esse é o momento de comprar.'
+
         msg_log = msg_log + alert
-        msg_email = msg_email + alert
-        send_email(msg_email)
-    elif lower_limit > price:
-        alert = '. Esse é o momento de comprar.'
-        msg_log = msg_log + alert
-        msg_email = msg_email + alert
-        send_email(msg_email)
+        email_msg = email_msg + alert
+
+        last_monitoring = get_last_monitoring(asset_name)
+
+        if last_monitoring is not None:
+            last_price = last_monitoring.price
+            if(not equals(last_price, price)):
+                send_email(email_msg)
+        else:
+            send_email(email_msg)
+    
+    return msg_log
+
+@shared_task(bind=True)
+def save_quote_monitoring(self, asset_name, row):
+    try:
+        security = get_security(row)
+        monitor = get_monitor(asset_name)
+    except ObjectDoesNotExist:
+        print('O ativo não está acessível')
+        return
+    except Exception:
+        print('Não foi possível acessar os dados da API.')
+        return
+
+    price = security.price
+    tradetime = timezone.make_aware(security.tradetime)
+
+    msg_log = handler_log(monitor, security, asset_name)
+        
+    Quotation.objects.create(
+        asset=monitor.asset, 
+        price=price, 
+        recording_date=timezone.now(), 
+        trade_date=tradetime
+    )
 
     return msg_log
